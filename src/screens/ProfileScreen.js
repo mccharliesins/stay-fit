cb4c714f30e7a1a7a8be65092bd572565acd682c
mccharliesins <vkyd1909@gmail.com>
2025-03-12 20:23:28 -0400

    fix: correct image picker media type option

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -108,7 +108,7 @@ const ProfileScreen = () => {
     try {
       console.log("Launching image picker...");
       const result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: [ImagePicker.MediaType.Images],
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
         aspect: [1, 1],
         quality: 0.8,
